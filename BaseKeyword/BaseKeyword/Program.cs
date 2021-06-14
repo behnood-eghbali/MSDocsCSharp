2a681e5f2b2f5c3da27026ea7964e7fc02edd39f
Behnood Eghbali <behnoodii@gmail.com>
2021-06-14 16:59:14 +0430

    add a new project

---
+++ BaseKeyword/BaseKeyword/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BaseKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee E = new Employee();
+            E.GetInfo();
+            Console.WriteLine();
+            DerivedClass md = new DerivedClass();
+            DerivedClass md1 = new DerivedClass(1);
+        }
+    }
+    /*
+    Output:
+    Name: John L. Malgraine
+    SSN: 444-55-6666
+    Employee ID: ABC567EFG
+    */
+
+    /*
+    Output:
+    in BaseClass()
+    in BaseClass(int i)
+    */
+}
