91333544191d3a896894b35483ca29d1cb175f72
Behnood Eghbali <behnoodii@gmail.com>
2021-06-15 09:18:27 +0430

    add a new project

---
+++ CopyConstructor/CopyConstructor/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructor
+{
+    class Person
+    {
+        // Copy constructor.
+        public Person(Person previousPerson)
+        {
+            Name = previousPerson.Name;
+            Age = previousPerson.Age;
+        }
+
+        // Alternate copy constructor calls the instance constructor.
+        //public Person(Person previousPerson)
+        //    : this(previousPerson.Name, previousPerson.Age)
+        //{
+        //}
+
+        // Instance constructor.
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public string Details()
+        {
+            return Name + " is " + Age.ToString();
+        }
+    }
+}
---
+++ CopyConstructor/CopyConstructor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CopyConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Person object by using the instance constructor.
+            Person person1 = new Person("George", 40);
+
+            // Create another Person object, copying person1.
+            Person person2 = new Person(person1);
+
+            // Change each person's age.
+            person1.Age = 39;
+            person2.Age = 41;
+
+            // Change person2's name.
+            person2.Name = "Charles";
+
+            // Show details to verify that the name and age fields are distinct.
+            Console.WriteLine(person1.Details());
+            Console.WriteLine(person2.Details());
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+    // Output:
+    // George is 39
+    // Charles is 41
+}
