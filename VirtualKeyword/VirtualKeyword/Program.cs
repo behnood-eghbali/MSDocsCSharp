caac1fbe5bb74b8230980b82e35bfb287054ff8e
Behnood Eghbali <behnoodii@gmail.com>
2021-06-15 10:00:48 +0430

    add a new project

---
+++ VirtualKeyword/VirtualKeyword/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VirtualKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Second Example");
+            Console.WriteLine();
+            double r = 3.0, h = 5.0;
+            Shape c = new Circle(r);
+            Shape s = new Sphere(r);
+            Shape l = new Cylinder(r, h);
+            // Display results.
+            Console.WriteLine("Area of Circle   = {0:F2}", c.Area());
+            Console.WriteLine("Area of Sphere   = {0:F2}", s.Area());
+            Console.WriteLine("Area of Cylinder = {0:F2}", l.Area());
+        }
+    }
+    /*
+    Output:
+    Area of Circle   = 28.27
+    Area of Sphere   = 113.10
+    Area of Cylinder = 150.80
+    */
+}
---
+++ VirtualKeyword/VirtualKeyword/SecondExample.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualKeyword
+{
+    public class Shape
+    {
+        public const double PI = Math.PI;
+        protected double x, y;
+
+        public Shape()
+        {
+        }
+
+        public Shape(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public virtual double Area()
+        {
+            return x * y;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(double r) : base(r, 0)
+        {
+        }
+
+        public override double Area()
+        {
+            return PI * x * x;
+        }
+    }
+
+    class Sphere : Shape
+    {
+        public Sphere(double r) : base(r, 0)
+        {
+        }
+
+        public override double Area()
+        {
+            return 4 * PI * x * x;
+        }
+    }
+
+    class Cylinder : Shape
+    {
+        public Cylinder(double r, double h) : base(r, h)
+        {
+        }
+
+        public override double Area()
+        {
+            return 2 * PI * x * x + 2 * PI * x * y;
+        }
+    }
+}
