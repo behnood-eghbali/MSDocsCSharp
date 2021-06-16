eb24b89715d51458915f5532a1e0cd5114e44015
Behnood Eghbali <behnoodii@gmail.com>
2021-06-16 08:18:41 +0430

    add a new project

---
+++ StaticConstructor/StaticConstructor/Bus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticConstructor
+{
+    class Bus
+    {
+        // Static variable used by all Bus instances.
+        // Represents the time the first bus of the day starts its route.
+        protected static readonly DateTime globalStartTime;
+
+        // Property for the number of each bus.
+        protected int RouteNumber { get; set; }
+
+        // Static constructor to initialize the static variable.
+        // It is invoked before the first instance constructor is run.
+        static Bus()
+        {
+            globalStartTime = DateTime.Now;
+
+            // The following statement produces the first line of output,
+            // and the line occurs only once.
+            Console.WriteLine("Static constructor sets global start time to {0}",
+                globalStartTime.ToLongTimeString());
+        }
+
+        // Instance constructor.
+        public Bus(int routeNum)
+        {
+            RouteNumber = routeNum;
+            Console.WriteLine("Bus #{0} is created.", RouteNumber);
+        }
+
+        // Instance method.
+        public void Drive()
+        {
+            TimeSpan elapsedTime = DateTime.Now - globalStartTime;
+
+            // For demonstration purposes we treat milliseconds as minutes to simulate
+            // actual bus times. Do not do this in your actual bus schedule program!
+            Console.WriteLine("{0} is starting its route {1:N2} minutes after global start time {2}.",
+                                    this.RouteNumber,
+                                    elapsedTime.Milliseconds,
+                                    globalStartTime.ToShortTimeString());
+        }
+    }
+}
---
+++ StaticConstructor/StaticConstructor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StaticConstructor
+{
+    // A static constructor is used to initialize any static data, or to perform a particular action that needs to be performed only once.
+    // It is called automatically before the first instance is created or any static members are referenced.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The creation of this instance activates the static constructor.
+            Bus bus1 = new Bus(71);
+
+            // Create a second bus.
+            Bus bus2 = new Bus(72);
+
+            // Send bus1 on its way.
+            bus1.Drive();
+
+            // Wait for bus2 to warm up.
+            System.Threading.Thread.Sleep(25);
+
+            // Send bus2 on its way.
+            bus2.Drive();
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+    /* Sample output:
+    Static constructor sets global start time to 3:57:08 PM.
+    Bus #71 is created.
+    Bus #72 is created.
+    71 is starting its route 6.00 minutes after global start time 3:57 PM.
+    72 is starting its route 31.00 minutes after global start time 3:57 PM.
+    */
+}
