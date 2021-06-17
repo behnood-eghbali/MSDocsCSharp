a3e2a342487875f5bbc7507fbf48bcba28f3d74c
Behnood Eghbali <behnoodii@gmail.com>
2021-06-17 10:37:07 +0430

    add a new project

---
+++ OverridingToString/OverridingToString/ObjectClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverridingToString
+{
+    public class ObjectClass
+    {
+        private object value;
+
+        public ObjectClass(object value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ": " + value.ToString();
+        }
+    }
+}
---
+++ OverridingToString/OverridingToString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OverridingToString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ObjectClass objInstance = new ObjectClass('a');
+            Console.WriteLine(objInstance.ToString());
+        }
+    }
+}
