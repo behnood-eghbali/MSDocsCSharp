a667a3b9c85982e304f97cc08a3bcf462d26d7f0
Behnood Eghbali <behnoodii@gmail.com>
2021-06-17 10:46:45 +0430

    add a new project

---
+++ ExtendingToString/ExtendingToString/List.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendingToString
+{
+    public class CList<T> : List<T>
+    {
+        public CList(IEnumerable<T> collection) : base(collection)
+        { }
+
+        public CList() : base()
+        { }
+
+        public override string ToString()
+        {
+            string retVal = string.Empty;
+            foreach (T item in this)
+            {
+                if (string.IsNullOrEmpty(retVal))
+                    retVal += item.ToString();
+                else
+                    retVal += string.Format(", {0}", item);
+            }
+            return retVal;
+        }
+    }
+}
---
+++ ExtendingToString/ExtendingToString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtendingToString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list2 = new CList<int>();
+            list2.Add(1000);
+            list2.Add(2000);
+            Console.WriteLine(list2.ToString());
+        }
+    }
+    // The example displays the following output:
+    //    1000, 2000
+}
