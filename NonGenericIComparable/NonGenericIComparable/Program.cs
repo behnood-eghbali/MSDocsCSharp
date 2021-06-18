20a041dabd261277685694f973cc22bc66269800
Behnood Eghbali <behnoodii@gmail.com>
2021-06-18 09:04:25 +0430

    add a new project

---
+++ NonGenericIComparable/NonGenericIComparable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace NonGenericIComparable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList temperatures = new ArrayList();
+            // Initialize random number generator.
+            Random rnd = new Random();
+
+            // Generate 10 temperatures between 0 and 100 randomly.
+            for (int ctr = 1; ctr <= 10; ctr++)
+            {
+                int degrees = rnd.Next(0, 100);
+                Temperature temp = new Temperature();
+                temp.Fahrenheit = degrees;
+                temperatures.Add(temp);
+            }
+
+            // Sort ArrayList.
+            temperatures.Sort();
+
+            foreach (Temperature temp in temperatures)
+                Console.WriteLine(temp.Fahrenheit);
+        }
+    }
+    // The example displays the following output to the console (individual
+    // values may vary because they are randomly generated):
+    //       2
+    //       7
+    //       16
+    //       17
+    //       31
+    //       37
+    //       58
+    //       66
+    //       72
+    //       95
+}
---
+++ NonGenericIComparable/NonGenericIComparable/Temperature.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericIComparable
+{
+    public class Temperature : IComparable
+    {
+        // The temperature value
+        protected double temperatureF;
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Temperature otherTemperature = obj as Temperature;
+            if (otherTemperature != null)
+                return this.temperatureF.CompareTo(otherTemperature.temperatureF);
+            else
+                throw new ArgumentException("Object is not a Temperature");
+        }
+
+        public double Fahrenheit
+        {
+            get
+            {
+                return this.temperatureF;
+            }
+            set
+            {
+                this.temperatureF = value;
+            }
+        }
+
+        public double Celsius
+        {
+            get
+            {
+                return (this.temperatureF - 32) * (5.0 / 9);
+            }
+            set
+            {
+                this.temperatureF = (value * 9.0 / 5) + 32;
+            }
+        }
+    }
+}
