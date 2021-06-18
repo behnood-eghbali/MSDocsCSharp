0d03023aa412dbc37afd59a821bb7d4d3fff10b1
Behnood Eghbali <behnoodii@gmail.com>
2021-06-18 09:12:40 +0430

    add a new project

---
+++ NonGenericIComparer/NonGenericIComparer/ReverserClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericIComparer
+{
+    public class ReverserClass : IComparer
+    {
+        // Call CaseInsensitiveComparer.Compare with the parameters reversed.
+        int IComparer.Compare(Object x, Object y)
+        {
+            return ((new CaseInsensitiveComparer()).Compare(y, x));
+        }
+    }
+}
