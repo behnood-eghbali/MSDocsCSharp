bcee33031445abc7628d2d23a6f993dcb8978608
Behnood Eghbali <behnoodii@gmail.com>
2021-06-19 09:47:58 +0430

    add a new project

---
+++ GenericIComparer/GenericIComparer/Box.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericIComparer
+{
+    public class Box : IComparable<Box>
+    {
+
+        public Box(int h, int l, int w)
+        {
+            this.Height = h;
+            this.Length = l;
+            this.Width = w;
+        }
+        public int Height { get; private set; }
+        public int Length { get; private set; }
+        public int Width { get; private set; }
+
+        public int CompareTo(Box other)
+        {
+            // Compares Height, Length, and Width.
+            if (this.Height.CompareTo(other.Height) != 0)
+            {
+                return this.Height.CompareTo(other.Height);
+            }
+            else if (this.Length.CompareTo(other.Length) != 0)
+            {
+                return this.Length.CompareTo(other.Length);
+            }
+            else if (this.Width.CompareTo(other.Width) != 0)
+            {
+                return this.Width.CompareTo(other.Width);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ GenericIComparer/GenericIComparer/BoxLengthFirst.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericIComparer
+{
+    public class BoxLengthFirst : Comparer<Box>
+    {
+        // Compares by Length, Height, and Width.
+        public override int Compare(Box x, Box y)
+        {
+            if (x.Length.CompareTo(y.Length) != 0)
+            {
+                return x.Length.CompareTo(y.Length);
+            }
+            else if (x.Height.CompareTo(y.Height) != 0)
+            {
+                return x.Height.CompareTo(y.Height);
+            }
+            else if (x.Width.CompareTo(y.Width) != 0)
+            {
+                return x.Width.CompareTo(y.Width);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+}
