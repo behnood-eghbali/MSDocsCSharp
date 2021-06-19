96c01de65af6f04cedc4a9292782187c80770dc6
Behnood Eghbali <behnoodii@gmail.com>
2021-06-19 10:02:38 +0430

    add a new project

---
+++ GetEnumeratorMethod/GetEnumeratorMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GetEnumeratorMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] peopleArray = new Person[3]
+{
+                new Person("John", "Smith"),
+                new Person("Jim", "Johnson"),
+                new Person("Sue", "Rabon"),
+};
+
+            People peopleList = new People(peopleArray);
+            foreach (Person p in peopleList)
+                Console.WriteLine(p.firstName + " " + p.lastName);
+        }
+    }
+    /* This code produces output similar to the following:
+    *
+    * John Smith
+    * Jim Johnson
+    * Sue Rabon
+    *
+    */
+}
