0becfee1c4a862487ea41869a6cf8efc42b2d8bd
Behnood Eghbali <behnoodii@gmail.com>
2021-06-21 12:21:21 +0430

    add a new project

---
+++ UserDefinedConversions/UserDefinedConversions/Digit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefinedConversions
+{
+    public readonly struct Digit
+    {
+        private readonly byte digit;
+
+        public Digit(byte digit)
+        {
+            if (digit > 9)
+            {
+                throw new ArgumentOutOfRangeException(nameof(digit), "Digit cannot be greater than nine.");
+            }
+            this.digit = digit;
+        }
+
+        public static implicit operator byte(Digit d) => d.digit;
+        public static explicit operator Digit(byte b) => new Digit(b);
+
+        public override string ToString() => $"{digit}";
+    }
+}
---
+++ UserDefinedConversions/UserDefinedConversions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserDefinedConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Digit(7);
+
+            byte number = d;
+            Console.WriteLine(number);  // output: 7
+
+            Digit digit = (Digit)number;
+            Console.WriteLine(digit);  // output: 7
+        }
+    }
+}
