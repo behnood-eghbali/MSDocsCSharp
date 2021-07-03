f7dfb79c49f71d689c1269c01add822d6da6a37f
Behnood Eghbali <behnoodii@gmail.com>
2021-07-03 14:00:55 +0430

    add a new project

---
+++ StaticKeyword/StaticKeyword/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticKeyword
+{
+    public class Employee
+    {
+        public string id;
+        public string name;
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+        public static int employeeCounter;
+
+        public static int AddEmployee()
+        {
+            return ++employeeCounter;
+        }
+    }
+}
---
+++ StaticKeyword/StaticKeyword/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StaticKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the employee's name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter the employee's ID: ");
+            string id = Console.ReadLine();
+
+            // Create and configure the employee object.
+            Employee e = new Employee(name, id);
+            Console.Write("Enter the current number of employees: ");
+            string n = Console.ReadLine();
+            Employee.employeeCounter = Int32.Parse(n);
+            Employee.AddEmployee();
+
+            // Display the new information.
+            Console.WriteLine($"Name: {e.name}");
+            Console.WriteLine($"ID:   {e.id}");
+            Console.WriteLine($"New Number of Employees: {Employee.employeeCounter}");
+        }
+    }
+    /*
+    Input:
+    Matthias Berndt
+    AF643G
+    15
+     *
+    Sample Output:
+    Enter the employee's name: Matthias Berndt
+    Enter the employee's ID: AF643G
+    Enter the current number of employees: 15
+    Name: Matthias Berndt
+    ID:   AF643G
+    New Number of Employees: 16
+    */
+}
