e5866ddd9c31f4556aa93acfff19babc7aa042fc
Behnood Eghbali <behnoodii@gmail.com>
2021-07-27 20:42:10 +0430

    add a new project

---
+++ TaskExecution/TaskExecution/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExecution
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await Task.Run(() => {
+                // Just loop.
+                int ctr = 0;
+                for (ctr = 0; ctr <= 1000000; ctr++)
+                { }
+                Console.WriteLine("Finished {0} loop iterations",
+                                ctr);
+            });
+        }
+    }
+    // The example displays the following output:
+    //        Finished 1000001 loop iterations
+}
